ae849ce30fe210f5dbf17e76b02a962ce4cbc6a5
Alex Booker <booker@booker.codes>
2020-08-16 16:39:59 +0100

    render static heatmap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React from "react";
 import "./styles.css";
 
-export default function App() {
+const RECT_SIZE = 10;
+
+const randomColor = () => {
+  const colors = ["red", "green", "orange", "purple", "pink", "yellow", "blue"];
+  return colors[Math.floor(Math.random() * colors.length)];
+};
+
+const renderDayRect = (day, week) => {
   return (
-    <div className="test">
-      <h1>Hello GitHub</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <rect
+      key={day}
+      width={RECT_SIZE}
+      height={RECT_SIZE}
+      fill={randomColor()}
+      x={0}
+      y={(day - 1) * RECT_SIZE}
+    >
+      <title>
+        week {week}, day {day}
+      </title>
+    </rect>
   );
-}
+};
+const renderWeekColumn = (week) => {
+  const dayCount = 7;
+  const days = Array.from(Array(dayCount), (_, i) => i + 1);
+  return (
+    <g key={week} transform={`translate(${(week - 1) * RECT_SIZE}, 0)`}>
+      {days.map((day) => renderDayRect(day, week))}
+    </g>
+  );
+};
+
+const renderHeatMap = () => {
+  const weekCount = 52;
+  const weeks = Array.from(Array(weekCount), (_, i) => i + 1);
+  const weekColumns = weeks.map((week) => renderWeekColumn(week));
+  return weekColumns;
+};
+
+export default () => (
+  <div>
+    <svg width={600} height={300}>
+      {renderHeatMap()}
+    </svg>
+  </div>
+);
